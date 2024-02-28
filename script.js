e4adde172c16a1fa2382c852986461c2a124d53e
Teun Jansen <teunjansen91@gmail.com>
2024-02-28 11:23:35 +0100

    addEntry() function bug fixes

--- script.js
+++ script.js
@@ -21,12 +21,14 @@ function addEntry() {
     `#${entryDropdown.value} .input-container`
   );
   const entryNumber =
-    targetInputContainer.querySelectorAll('input[type="text"]').length;
+    targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
     <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
     <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
     <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories"></input>
   `;
-  targetInputContainer.innerHTML += HTMLString;
+  targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
+
+addEntryButton.addEventListener("click", addEntry);
